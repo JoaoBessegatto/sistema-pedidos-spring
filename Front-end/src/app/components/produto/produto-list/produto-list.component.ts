e45e330b8ded125794c4258e62017eb801b2444c
João Pedro Bessegatto <bfjoaop@gmail.com>
2025-05-04 23:45:07 -0300

    Adicão de Id no DTO response de produtos

--- Front-end/src/app/components/produto/produto-list/produto-list.component.ts
+++ Front-end/src/app/components/produto/produto-list/produto-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Produto } from '../../../model/produto';
 import { ProdutoService } from '../../../service/produto-services';
 import { CommonModule } from '@angular/common';
-
+import { CarrinhoService } from '../../../service/carrinho.service';
 
 @Component({
   selector: 'app-produtos',
@@ -13,7 +13,9 @@ import { CommonModule } from '@angular/common';
 export class ProdutosComponent implements OnInit {
   produtos: Produto[] = [];
 
-  constructor(private produtoService: ProdutoService) {}
+  constructor(private produtoService: ProdutoService,
+      private carrinhoService: CarrinhoService
+  ) {}
 
   ngOnInit(): void {
     this.produtoService.getAll().subscribe({
@@ -24,5 +26,10 @@ export class ProdutosComponent implements OnInit {
         console.error('Erro ao buscar produtos:', err);
       }
     });
+  }
+  adicionarAoCarrinho(produto: Produto): void {
+    this.carrinhoService.adicionarAoCarrinho(produto);
+    alert('Produto adicionado ao carrinho!');
+    console.log(this.produtos);
   }
 }
