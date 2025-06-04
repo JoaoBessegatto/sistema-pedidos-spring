07d2b127187363a380aed7bff1faeb361e4c34a0
joao-pedro <bfjoaop@gmail.com>
2025-06-03 22:32:51 -0300

    New: Mudança no metodo logar, adição de autenticação JWT

--- Front-end/src/app/service/produto-services.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProdutoServices } from './produto-services';
-
-describe('ProdutoServices', () => {
-  it('should create an instance', () => {
-    expect(new ProdutoServices()).toBeTruthy();
-  });
-});
